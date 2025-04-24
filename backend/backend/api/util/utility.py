8b5102dc0ad9e59ae6fe82546d68b32f3e968144
Igor Melnikov <igormeln2003@gmail.com>
2025-04-24 23:08:43 +0300

    синхронизация с актуальной версией

--- backend/backend/api/util/utility.py
+++ backend/backend/api/util/utility.py
@@ -1,7 +1,7 @@
-from model.resource import Resource
-from config.config import ServerConfig, S3Config
-from util.cron import create_cron_job, update_cron_job, kill_cron_job
-from model.s3_interactor import get_all_files
+from api.model.resource import Resource
+from api.config.config import ServerConfig, S3Config
+from api.util.cron import create_cron_job, update_cron_job, kill_cron_job
+from api.model.s3_interactor import get_all_files
 from datetime import datetime
 from typing import List, Tuple
 from selenium import webdriver
